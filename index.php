f4503ab91bbebd1b04fcfae114821b9b20f62d22
Javier <jmartinezcampos98@gmail.com>
2021-02-17 12:43:01 +0100

    Posicionando puntos

--- index.php
+++ index.php
@@ -35,7 +35,11 @@
                 $xCoord = $punto["xCoord"];
                 $yCoord = $punto["yCoord"];
                 $color = $punto["color"];
-                echo ('<span class="dot punto_redondo" style="background-color: ' . $color . ';top: ' . $yCoord . 'px;left: ' . $xCoord . 'px;"></span>');
+                $nombre = $punto["nombre"];
+                echo('<div>');
+                echo('<span class="dot punto_redondo" style="background-color: ' . $color . ';top: ' . $yCoord . 'px;left: ' . $xCoord . 'px;"></span>');
+                echo('<tag class="puntos" style="top:' . $yCoord . 'px; left: ' . $xCoord + 15 . 'px;">' . $nombre . '</tag>');
+                echo('</div>');
             }
         ?>
         <form action="proceso_guardar.php" method="POST" enctype="multipart/form-data">
@@ -44,30 +48,11 @@
             <input type="file" required name="imagen"/>
             <input type="submit" value="Aceptar"/>
         </form>
+        <button onclick="crear()">Crear punto</button>
+        <button>Actualizar puntos</button>
 
 
     </div>
-    <div style="display: grid; grid-template-columns: 250px 200px">
-        <div style="grid-column: 1">
-            <form id="form" action="/action_page.php">
-                <div id="form1">
-
-                    <br>
-                </div>
-                <br>
-            </form>
-
-            <h3>un texto</h3>
-
-            <button onclick="comprobar()">Actualizar</button>
-
-        </div>
-    </div>
-    <button>Mostrar/Ocultar</button>
-    <button onclick="crear()">Crear punto</button>
-    <br>
-    <input type='file' onchange="readURL(this);" />
-    <br>
 
     <input id='input_x' style="width: 50px; height:20px;"> </input>
     <input id="input_y" style="width: 50px; height:20px;"> </input>
